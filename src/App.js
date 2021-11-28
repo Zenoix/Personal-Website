046adbf28871bd2562091c79d38761486e7a50f5
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-11-28 21:07:59 +1300

    Created component files and got multipage working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Navbar, Footer, Home, About, Projects, Contacts } from "./components";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Site currently under development</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./components/Navbar";
+export { default as Footer } from "./components/Footer";
+export { default as Home } from "./components/Home";
+export { default as About } from "./components/About";
+export { default as Projects } from "./components/Projects";
+export { default as Contacts } from "./components/Contacts";
