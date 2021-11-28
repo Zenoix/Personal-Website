dd7314f26195e5ffd75952d9d064de382285f097
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-11-29 04:28:32 +1300

    Started working on navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,5 @@
 const Footer = () => {
-  return <footer>
-    Footer
-    </footer>;
+  return <footer className="w-full py-2 md:py-4"><span>&copy;</span> Jeff Wang 2021</footer>;
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,61 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import HamburgerIcon from "@material-ui/icons/Menu";
 
 const Navbar = () => {
+  const [isHamburgerActive, setHamburgerActive] = useState("false");
+
+  const handleToggle = () => {
+    setHamburgerActive(!isHamburgerActive);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/'>Home</NavLink>
-        </li>
-        <li>
-          <NavLink to='/about'>About</NavLink>
-        </li>
-        <li>
-          <NavLink to='/projects'>Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to='/contacts'>Contacts</NavLink>
-        </li>
-      </ul>
+    <nav className='flex flex-wrap items-center justify-between w-full py-3 md:py-0 px-4 text-lg'>
+      <div className="px-6 md:px-8">
+        <h2 className="font-bold">
+          <NavLink to='/'>jeffwang.py</NavLink>
+        </h2>
+      </div>
+      <div className='h-6 w-6 cursor-pointer md:hidden block'
+        onClick={handleToggle}><HamburgerIcon
+        
+      /></div>
+      
+      <div
+        className={`${
+          isHamburgerActive ? "hidden" : ""
+        }  w-full md:flex md:items-center md:w-auto`}
+      >
+        <ul
+          className='
+              pt-4
+              text-base
+              md:flex
+              md:justify-between 
+              md:pt-0'
+        >
+          <li>
+            <NavLink className='px-6 md:p-4 py-2 block font-normal hover:text-accent-pink' to='/'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className='px-6 md:p-4 py-2 block font-normal hover:text-accent-pink' to='/about'>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className='px-6 md:p-4 py-2 block font-normal hover:text-accent-pink' to='/projects'>
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className='px-6 md:p-4 py-2 block font-normal hover:text-accent-pink' to='/contacts'>
+              Contacts
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
