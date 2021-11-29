5a8fddb7990de20f0c7caa3c3d854c718b2c24bd
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-11-30 04:06:15 +1300

    Added content to front page and changed how button text worked

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,24 @@
 import { NavLink } from "react-router-dom";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
-const Button = ({page}) => {
-    return (
-        <div className="button px-8 py-2 bg-accent-pink text-center inline-block">
-            <NavLink
-              className="font-semibold"
-              to={`/${page}`}
-            >{page}</NavLink>
-        </div>
-    )
-}
+const Button = ({ buttonName, page }) => {
+  return (
+    <div className='button px-8 py-2 bg-accent-pink text-center inline-block'>
+      <NavLink className='font-semibold' to={`/${page}`}>
+        {buttonName}
+      </NavLink>
+    </div>
+  );
+};
 
 Button.defaultProps = {
-    page: "404"
-}
+  buttonName: "Unavailable",
+  page: "404",
+};
 
 Button.propTypes = {
-    page: PropTypes.string.isRequired,
-}
+  page: PropTypes.string.isRequired,
+  buttonName: PropTypes.string.isRequired,
+};
 
-export default Button
+export default Button;
