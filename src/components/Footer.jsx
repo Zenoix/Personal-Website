a849c9bb4498d4b02e65ba77222f9c4ee605daf2
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-11-30 02:47:02 +1300

    Landing page V1 done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer className="w-full">
-    <div className="waves waves-small md:hidden"></div>
-    <div className="hidden waves waves-reg md:block 3xl:hidden"></div>
-    <div className="hidden waves waves-large 3xl:block"></div>   
-      <div className="footer-bar py-2 text-sm	 md:py-3 md:text-base">
+    <footer className='w-full mt-8'>
+      <div className='waves waves-small md:hidden'></div>
+      <div className='hidden waves waves-reg md:block 3xl:hidden'></div>
+      <div className='hidden waves waves-large 3xl:block'></div>
+      <div className='footer-bar py-2 text-sm	 md:py-3 md:text-base'>
         <span>&copy;</span> Jeff Wang 2021
       </div>
     </footer>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,16 @@
 import Terminal from "./Terminal";
+import LandingCards from "./LandingCards";
 
 const Home = () => {
+  const aboutContent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, veritatis inventore voluptatem consectetur quibusdam velit vitae tempore eveniet perspiciatis corrupti veniam tempora molestias officiis aperiam voluptas quasi a ad culpa!"
   return (
-    <main className="flex flex-col justify-center md:grid md:grid-cols-12 md:mx-14 md:gap-x-5 lg:mx-52">
+    <main className='flex flex-col justify-center md:grid md:grid-cols-12 md:mx-14 md:gap-x-5 lg:mx-52'>
       <Terminal />
+      <section className="mx-12">
+        <LandingCards heading="Hi!, I'm Jeff" content={aboutContent} page="About"/>
+        <LandingCards heading="My Latest Project" page="Projects"/>
+        <LandingCards heading="How to Contact Me" page="Contacts"/>
+      </section>
     </main>
   );
 };
