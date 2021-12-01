b9f79cc0bf8a99c6513c538a4bb3c829316d6381
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-12-02 02:06:44 +1300

    New breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,30 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "media", // or 'media' or 'class'
   theme: {
-    extend: {
-      screens: {
-        "3xl": "1921px",
-      },
+    screens: {
+      "2xs": "320px",
+      // => @media (min-width: 320px) { ... }
+
+      xs: "425px",
+      // => @media (min-width: 425px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+      
+      "3xl": "1921px",
+      // => @media (min-width: 1921px) { ... }
     },
     colors: {
       transparent: "transparent",
