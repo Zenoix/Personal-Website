064c26882a49ad93e7bb829460b8b4bcc4938024
Zenoix <41812358+Zenoix@users.noreply.github.com>
2021-12-08 00:18:04 +1300

    Cleaned up terminal code

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -10,14 +10,14 @@ const Terminal = () => {
         <Add className='term-button mx-1' id='term-max' />
         <Close className='term-button mx-1 mr-2' id='term-close' />
       </div>
-      <div className='terminal flex flex-row p-6 xs:justify-center xs:py-16 sm:py-24 lg:px-18 lg:py-24'>
-        <span className='overflow-hidden path relative -top-2 block text-2xl xs:text-4xl sm:text-5xl md:text-6xl xl:leading-snug sm:tracking-tight lg:tracking-tighter'>
+      <div className='terminal flex flex-row p-6 xs:justify-center xs:py-16 sm:py-24 lg:px-18 lg:py-24 text-2xl xs:text-4xl sm:text-5xl md:text-6xl xl:leading-snug sm:tracking-tight lg:tracking-tighter'>
+        <span className='overflow-hidden path relative -top-2 block'>
           ~
         </span>
-        <span className='overflow-hidden reg-prompt relative -top-1 block text-2xl xs:text-4xl sm:text-5xl md:text-6xl xl:leading-snug sm:tracking-tight lg:tracking-tighter px-3'>
+        <span className='overflow-hidden reg-prompt relative -top-1 block px-3'>
           ➜
         </span>
-        <span className='term-text overflow-hidden block text-2xl xs:text-4xl sm:text-5xl md:text-6xl xl:leading-snug sm:tracking-tight lg:tracking-tighter'>
+        <span className='term-text overflow-hidden block'>
           Hi! I'm <span style={{ color: "#6be6e6" }}>Jeff</span>
         </span>
       </div>
